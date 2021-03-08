78c6a126ffd9b8202fb4003db010cc8e40894230
echosoar <82163514@qq.com>
2021-03-08 20:23:41 +0800

    fix: new path

--- bin/create-midway.js
+++ bin/create-midway.js
@@ -2,5 +2,8 @@
 'use strict';
 const minimist = require('minimist');
 const argv = minimist(process.argv.slice(2));
+if (argv._[0] === 'init') {
+  argv._ = argv._.slice(1);
+}
 const { cli } = require('./cli');
 cli(argv);
