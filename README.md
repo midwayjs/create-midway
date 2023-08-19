fa5289361cf75ce2c8b1e25973361f25a9ebbcbb
Harry Chen <czy88840616@gmail.com>
2023-08-19 23:11:35 +0800

    feat: remove cli

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # create-midway
+
 npm init midway
+
+# 扩展
+
+如果你希望扩展，可以传递参数。
+
+```js
+'use strict';
+const { AddPlugin } = require('create-midway');
+
+new AddPlugin().run({
+  npm: 'tnpm',
+  templateList: {
+    // 模版列表
+  }
+}).catch(e => {console.error(e)});
+```
--- bin/create-midway.js
+++ bin/create-midway.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
 'use strict';
-const minimist = require('minimist');
-const argv = minimist(process.argv.slice(2));
-if (process.env.FAAS_CLI_VERBOSE || argv.V) {
-  console.log(argv);
-}
-if (argv._[0] === 'init') {
-  argv._ = argv._.slice(1);
-}
-if (argv._[0] === 'midway') {
-  argv._ = argv._.slice(1);
-}
-const { cli } = require('./cli');
-cli(argv).catch(err => {
-  console.error(err);
-})
+const { AddPlugin } = require('../dist/index');
+new AddPlugin().run().catch(e => {console.error(e)});
